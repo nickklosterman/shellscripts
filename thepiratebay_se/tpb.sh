38efaecc2af80fef9f1912c18436f397420c0a19
nick klosterman <nick_klosterman@yahoo.com>
2013-10-28 22:04:10 -0400

    Added category selection, magnets are sent to a file instead of the program

--- thepiratebay_se/tpb.sh
+++ thepiratebay_se/tpb.sh
@@ -5,16 +5,26 @@
 # http://www.opensource.org/licenses/gpl-3.0.html
 #
 
+
+#TODO:
+# get the magnet links of the files and autoload them into rtorrent
+# or run them through the magnet->.torrent file generator and stick them in the watch folder
+# option to output , errr download the torrent files
+
+
 ### CONFIGURATION ###
 # program to use for torrent download
 # magnet link to torrent will be appended
 # you can add -- at the end to indicate end of options
 # (if your program supports it, most do)
 program='/usr/bin/transmission-remote -a'
-TPB="https://thepiratebay.se"
+TPB="https://thepiratebay.sx"
 
 # show N first matches by default
 limit=15
+
+#default to search and display results for all categories
+category=0
 
 # colors
 numbcolor='\x1b[1;35m'
@@ -35,17 +45,133 @@ nonecolor='\x1b[0m'
 orderby=7
 ### END CONFIGURATION ###
 
+outputMagnetFile=TPB.magnets
+
+constants() {
+echo="
+class ORDERS(Constants):
+    class NAME:
+        ASC = 1
+        DES = 2
+    class UPLOADED:
+        ASC = 3
+        DES = 4
+    class SIZE:
+        ASC = 5
+        DES = 6
+    class SEEDERS:
+        ASC = 7
+        DES = 8
+    class LEECHERS:
+        ASC = 9
+        DES = 10
+    class UPLOADER:
+        ASC = 11
+        DES = 12
+    class TYPE:
+        ASC = 13
+        DES = 14
+
+class CATEGORIES(Constants):
+    ALL = 0
+    class AUDIO:
+        ALL = 100
+        MUSIC = 101
+        AUDIO_BOOKS = 102
+        SOUND_CLIPS = 103
+        FLAC = 104
+        OTHER = 199
+    class VIDEO:
+        ALL = 200
+        MOVIES = 201
+        MOVIES_DVDR = 202
+        MUSIC_VIDEOS = 203
+        MOVIE_CLIPS = 204
+        TV_SHOWS = 205
+        HANDHELD = 206
+        HD_MOVIES = 207
+        HD_TV_SHOWS = 208
+        THREE_DIMENSIONS = 209
+        OTHER = 299
+    class APPLICATIONS:
+        ALL = 300
+        WINDOWS = 301
+        MAC = 302
+        UNIX = 303
+        HANDHELD = 304
+        IOS = 305
+        ANDROID = 306
+        OTHER = 399
+    class GAMES:
+        ALL = 400
+        PC = 401
+        MAC = 402
+        PSX = 403
+        XBOX360 = 404
+        WII = 405
+        HANDHELD = 406
+        IOS = 407
+        ANDROID = 408
+        OTHER = 499
+    class OTHER:
+	ALL = 600
+        EBOOKS = 601
+        COMICS = 602
+        PICTURES = 603
+        COVERS = 604
+        PHYSIBLES = 605
+        OTHER = 699
+
+
+"
+}
+listCategories() {
+echo -e "ALL = 0(default)
+    class AUDIO:
+        ALL = 100
+        MUSIC = 101
+        AUDIO_BOOKS = 102
+        SOUND_CLIPS = 103
+        FLAC = 104
+        OTHER = 199
+    class VIDEO: \t\tclass GAMES:
+        ALL = 200 \t\t\tALL = 400
+        MOVIES = 201\t\t\tPC = 401
+        MOVIES_DVDR = 202\t\tMAC = 402
+        MUSIC_VIDEOS = 203\t\tPSX = 403
+        MOVIE_CLIPS = 204\t\tXBOX360 = 404
+        TV_SHOWS = 205\t\t\tWII = 405
+        HANDHELD = 206\t\t\tHANDHELD = 406
+        HD_MOVIES = 207\t\t\tIOS = 407
+        HD_TV_SHOWS = 208\t\tANDROID = 408
+        THREE_DIMENSIONS = 209\t\tOTHER = 409
+        OTHER = 299
+    class APPLICATIONS:\t\tclass OTHER:
+        ALL = 300\t\t\tALL = 600
+        WINDOWS = 301\t\t\tEBOOKS  = 601
+        MAC = 302\t\t\tCOMICS  = 602
+        UNIX = 303\t\t\tPICTURES  = 603
+        HANDHELD = 304\t\t\tCOVERS  = 604
+        IOS = 305\t\t\tPHYSIBLES  = 605
+        ANDROID = 306\t\t\tOTHER  = 699
+        OTHER = 399
+"
+}
+
 thisfile="$0"
 
 printhelp() {
 	echo -e "Usage:"
 	echo -e "\t$thisfile [options] search query"
+	echo -e "\t$thisfile -c600 -n10 Linux"
 	echo
 	echo
 	echo -e "Available options:"
 	echo -e "\t-h\t\tShow help"
 	echo -e "\t-n [num]\tShow only first N results (default 15; max 100 [top] or 30 [search])"
 	echo -e "\t-C\t\tDo not use colors"
+	echo -e "\t-c [num]\tSearch by category"
+	echo -e "\t-L \t\tList categories"
 	echo -e "\t-P [prog]\tSet torrent client command (\`-P torrent-client\` OR \`-P \"torrent-client --options\"\`)"
 	echo
 	echo -e "Current client settings: $program [magnet link]"
@@ -64,17 +190,29 @@ chex() {
 }
 
 # script cmdline option handling
-while getopts :hn:CP:: opt ; do
+##while getopts :hn:c:CP:: opt ; do
+while getopts "hf:n:c:CLP:" opt ; do
 	case "$opt" in
 		h) printhelp; exit 0;;
 		n) limit="$OPTARG";;
 		C) unset numbcolor namecolor sizecolor seedcolor peercolor nonecolor errocolor mesgcolor;;
+		c) category="$OPTARG";;
+		L) listCategories;exit;;
 		P) program="$OPTARG"; chex;;
-		*) echo -e "Unknown option(s)."; printhelp; exit 1;;
+	        f) outputMagnetFile="$OPTARG";;
+		*) echo -e "Unknown option(s)."; printhelp; exit 1;;		
 	esac
 done
-
+#echo "category:$category \n limit:$limit \n program:$program"
 shift `expr $OPTIND - 1`
+
+#I think I won't check if there is a filename conflict. I think this will keep things simple. As it stands this will APPEND to a previous queries file.
+if [ $outputMagnetFile == "TPB.magnets" ]
+then
+    unencodedQuery=$*
+    outputMagnetFile="${unencodedQuery}.magnets"
+    echo ${outputMagnetFile}
+fi
 
 # correctly encode query
 q=`echo "$*" | tr -d '\n' | od -t x1 -A n | tr ' ' '%'`
@@ -83,9 +221,10 @@ q=`echo "$*" | tr -d '\n' | od -t x1 -A n | tr ' ' '%'`
 if [ -z "$q" ] ; then
 	url="top/all"
 else
-	url='search/'"$q"'/0/'"$orderby"'/0'
+	url='search/'"$q"'/0/'"$orderby"'/'"$category"
+	echo "${url}"
+#	url='search/'"$q"'/0/'"$orderby"'/600'
 fi
-
 # get results
 # Here be dragons!
 r=`curl -k -A Mozilla -b "lw=s" -m 15 -s "$TPB/$url" \
@@ -134,6 +273,7 @@ done
 # normalize download list, sort it and remove dupes
 down="$(echo $down | tr '\ ' '\n' | sort -n | uniq)"
 IFS=$'\n'
+#$down holds the index of the torrent to be downloaded
 
 # check whether we're downloading something, else exit
 if [ -z "$down" ] ; then
@@ -147,7 +287,9 @@ for torrent in $down ; do
 	if [ $torrent -ge 1 ] ; then
 		if [ $torrent -le $limit ] ; then
 			echo -n "$torrent "
-			command="$program `echo "$r" | awk -F '|' 'NR=='$torrent'{print $2; exit}'`"
+			#uncomment the following line to send the magnets to the desired program
+			#command="$program `echo "$r" | awk -F '|' 'NR=='$torrent'{print $2; exit}'`"
+			echo "$r" | awk -F '|' 'NR=='$torrent'{print $2; exit}' >> "${outputMagnetFile}"
 			status=$(eval "$command" 2>&1)
 			if [ $? -ne 0 ] ; then
 				echo -n '(failed!) '
